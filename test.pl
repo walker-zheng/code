50c3aa56e2f4a7cfcfd6ad0a3f47db5ab0a29e26
waler-zheng <mykulou@gmail.com>
2012-07-17 21:10:07 +0800

    add perl test

---
+++ test.pl
@@ -0,0 +1,18 @@
+#! /usr/bin/perl -w
+
+use strict;
+
+sub get_test{
+    my ($what, @array)=@_;
+    printf "".("%D\t" x @array),@array;
+    
+
+}
+
+
+sub main_test{
+    &get_test("haha", 1, 2, 3, 4);
+    
+
+}
+&main_test;
