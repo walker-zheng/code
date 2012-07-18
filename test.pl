538432be4312c0858a50b631c02089312fa2666f
waler-zheng <mykulou@gmail.com>
2012-07-18 22:00:44 +0800

    nothig

--- test.pl
+++ test.pl
@@ -5,14 +5,11 @@ use strict;
 sub get_test{
     my ($what, @array)=@_;
     printf "".("%D\t" x @array),@array;
-    
-
 }
 
 
 sub main_test{
     &get_test("haha", 1, 2, 3, 4);
     
-
 }
 &main_test;
