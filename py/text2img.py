bb37c4af6024c2668d91ee9bd9867bbba669e1ec
walker.zheng <walker.zheng@inforun.net>
2017-03-09 10:30:10 +0800

    添加 平滑处理，大字体书写，缩小保存
    Signed-off-by: walker.zheng <walker.zheng@inforun.net>

--- py/text2img.py
+++ py/text2img.py
@@ -55,25 +55,38 @@ def generate_pic():
     text = u'获取测试文本长度哈哈'
     line_len = len(text)
     fontSize = 15
+    fontHeight = 200             # 35 40 50 # 单字高度 越大字越清
+    picEdge = 1600             # 240 # 图片边长 单行字数 = picEdge/fontHeight
+    dst_scale = 240/picEdge
     width_plus = 10
     height_plus = 10
     radius_default = 0.5
+    color_white = (255, 255, 255, 255)
+    color_gray = (204, 204, 204, 255)
+    color_black = (0, 0, 0, 0)
+    isSmoooth = True
+    hasBackgroud = False
+    color_bg = None
+    color_fg = color_black
+    if hasBackgroud:
+        color_bg = color_gray
+        color_fg = color_white
     if not os.path.exists("images/"):
         os.mkdir("images/")
     for _,_,filenames in os.walk(fontPath):
         fontCount = 0
         for filename in filenames:
             font = pygame.font.Font(os.path.join("fonts", filename), fontSize)
-            _rtext = font.render(text, False, (255, 255, 255), (204, 204, 204))
+            _rtext = font.render(text, isSmoooth, color_fg, color_bg)
             _width, _height = _rtext.get_size()
-            while _height < 40:
+            while _height < fontHeight:
                 fontSize += 1
                 font = pygame.font.Font(os.path.join("fonts", filename), fontSize)
-                _rtext = font.render(text, False, (255, 255, 255), (204, 204, 204))
+                _rtext = font.render(text, isSmoooth, color_fg, color_bg)
                 _width, _height = _rtext.get_size()
             fontCount += 1
             width_one = _width/len(text)
-            line_len = math.floor(300/width_one)
+            line_len = math.floor(picEdge/width_one)
             #imagePath = "images/" + str(fontCount)
             imagePath = "images/" + os.path.splitext(filename)[0]
             if not os.path.exists(imagePath):
@@ -92,21 +105,22 @@ def generate_pic():
                     lines = splitByLen(line, line_len)
                 rtext1 = pygame.Surface((width_one * len(lines[0]) + width_plus * 2, _height * len(lines) + height_plus * 2), pygame.SRCALPHA)
                 rtext1.set_alpha(0)
-                AAfilledRoundedRect(rtext1, rtext1.get_rect(), (204, 204, 204), 0.5)
+                if hasBackgroud:
+                    AAfilledRoundedRect(rtext1, rtext1.get_rect(), color_bg, 0.5)
                 line_count = 0
                 for every in lines:
-                    ##  rtext = pygame.display.set_mode((300,300))
-                    rtext = font.render(every, False, (255, 255, 255), (204, 204, 204))
+                    rtext = font.render(every, isSmoooth, color_fg, color_bg)
                     rtext1.blit(rtext, (height_plus, width_plus + line_count * _height))
                     line_count += 1
+                pygame.image.save(rtext1, imagePath + "/" + str(count).zfill(2) + ".png")
                 count += 1
-                width_save = 240
-                height_save = math.floor(240*rtext1.get_height()/rtext1.get_width())
-                rtext2 = pygame.transform.scale(rtext1, (width_save, height_save))
-                rtext3 = pygame.Surface((240, 240), pygame.SRCALPHA)
+                width_save = math.floor(picEdge*dst_scale)
+                height_save = math.floor(picEdge*rtext1.get_height()/rtext1.get_width()*dst_scale)
+                rtext2 = pygame.transform.smoothscale(rtext1, (width_save, height_save))
+                rtext3 = pygame.Surface((picEdge*dst_scale, picEdge*dst_scale), pygame.SRCALPHA)
                 rtext3.set_alpha(0)
-                rtext3.blit(rtext2, (0, (240 - rtext2.get_height())/2))
+                rtext3.blit(rtext2, (0, (picEdge - rtext2.get_height())/2*dst_scale))
                 pygame.image.save(rtext3, imagePath + "/big/" + str(count).zfill(2) + ".png")
-                rtext2 = pygame.transform.scale(rtext3, (math.floor(rtext3.get_width()/2), math.floor(rtext3.get_height()/2)))
+                rtext2 = pygame.transform.smoothscale(rtext3, (math.floor(rtext3.get_width()/2*dst_scale), math.floor(rtext3.get_height()/2*dst_scale)))
                 pygame.image.save(rtext2, imagePath + "/small/" + str(count).zfill(2) + ".png")
 generate_pic()
