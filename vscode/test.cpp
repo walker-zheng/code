9cc6c31c291d5c439dfa2ca9c00a5b46494323c6
walker.zheng <walker.zheng@inforun.net>
2017-06-08 23:33:04 +0800

    add threadpool test
    Signed-off-by: walker.zheng <walker.zheng@inforun.net>

--- vscode/test.cpp
+++ vscode/test.cpp
@@ -1,22 +1,43 @@
 #include <iostream>
 #include <vector>
-#include <thread>
 #include <future>
+#include <chrono>
+#include "ThreadPool.h"
 
 using namespace std;
 
 int main()
 {
-    std::vector<std::thread> pool;
-    for(auto i : {1, 2, 3})
-        pool.emplace_back([=]()
+    auto timeout = std::chrono::seconds(2);
+    ThreadPool pool(4);
+    std::vector<std::future<int>> result;
+    for(auto i : {1, 2, 3, 4, 5})
+        result.emplace_back(pool.enqueue([=]()
         { 
+            // std::cout << std::to_string(i) + " start\n";
             std::this_thread::sleep_for(std::chrono::seconds(i)); 
-            std::cout << "hello, world "<< i << std::endl;
-        });
-    for(auto & task: pool)
+            // std::cout << std::to_string(i) + " done\n";
+            return i;
+        }));
+    int i = 0;
+    for(auto & fut: result)
     {
-        task.join();
+        i++;
+        std:async(std::launch::async, [&]()
+        {
+            auto status = fut.wait_for(timeout);
+            if( status == std::future_status::ready)
+            {
+                std::cout << std::to_string(fut.get()) + " result\n";
+            }
+            else if (status == std::future_status::timeout) 
+            {
+                std::cout <<  std::to_string(i) + " timeout\n";
+            }
+            else if (status == std::future_status::deferred) {
+                std::cout << std::to_string(i) + " deferred\n";
+            } 
+        });
     }
     return 0;
 }
