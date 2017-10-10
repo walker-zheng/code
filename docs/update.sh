71eb47f05de743a1eaa32363e156fe2a4b259108
walker.zheng <walker.zheng@inforun.net>
2017-10-10 13:51:12 +0800

    update master

--- docs/update.sh
+++ docs/update.sh
@@ -5,4 +5,4 @@
 
 (echo '# SUMMARY';echo '* [简介](README.md)';echo '* [目录](SUMMARY.md)';find wiki -name '*.md'|sed 's,\(.*\)\.md,* [\2](\2.md),') >SUMMARY.md
 git add . && git commit -m 'update master' 
-gitbook build && git checkout gh-pages && rm *.html && cp _book/*.html . && cp -R _book/gitbook . && git add . && git commit -m 'update pages' && git checkout master && git push --all
+gitbook build && rm *.html && cp _book/*.html . && cp -R _book/gitbook . && git add . && git commit -m 'update docs' && git push --all
