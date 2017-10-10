9bf1c02db247009e5104a14233afa17381c32284
walker.zheng <walker.zheng@inforun.net>
2017-10-10 14:00:49 +0800

    update master

--- docs/update.sh
+++ docs/update.sh
@@ -3,6 +3,6 @@
 #gitbook install && gitbook init        # 第一次执行时候
 
 
-(echo '# SUMMARY';echo '* [简介](README.md)';echo '* [目录](SUMMARY.md)';ls *.md|sed 's,\(.*\)\.md,* [\1](\1.md),') >SUMMARY.md
+(echo '# SUMMARY';echo '* [简介](README.md)';echo '* [目录](SUMMARY.md)';ls *.md|sed 's,\(.*\)\.md,* [\1](\1.md),') |grep -vE 'README|SUMMARY' > SUMMARY.md
 git add . && git commit -m 'update master' 
 gitbook build && rm *.html && cp _book/*.html . && cp -R _book/gitbook . && git add . && git commit -m 'update docs' && git push --all
