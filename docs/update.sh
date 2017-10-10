6bbb65695fc77e474ad5564a64835e5ac3f00d45
walker.zheng <walker.zheng@inforun.net>
2017-10-10 14:23:33 +0800

    update master

--- docs/update.sh
+++ docs/update.sh
@@ -5,4 +5,4 @@
 
 (echo '# SUMMARY';echo '* [简介](README.md)';echo '* [目录](SUMMARY.md)';ls *.md|sed 's,\(.*\)\.md,* [\1](\1.md),'|grep -vE 'README|SUMMARY') > SUMMARY.md
 git add . && git commit -m 'update master' 
-rm -rf {_book/,}*.html && gitbook build && cp _book/*.html . && cp -R _book/gitbook . && git add . && git commit -m 'update docs' && git push --all
+rm -rf {_book/,}*.html && gitbook build && cp _book/*.{html,json} . && cp -R _book/gitbook . && git add . && git commit -m 'update docs' && git push --all
