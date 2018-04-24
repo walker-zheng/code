47bd8578c0f83d2b7ec12b218abdc243914c799f
walker-zheng <mykulou@gmail.com>
2018-04-24 22:13:55 +0800

    update

--- ThreadPool/example.cpp
+++ ThreadPool/example.cpp
@@ -4,28 +4,34 @@
 #include <string>
 #include <future>
 #include <numeric>
+#include <thread>
+#include <algorithm>
 
 #include "ThreadPool.h"
 
+int sum_all(int n){
+    int sum{0};
+    for(auto i = 0; i != n; i++)
+        sum +=i;
+    return sum;
+}
 int main()
 {
-    
-    ThreadPool pool(4);
-    std::vector<std::future<int>> results;
-
+    std::vector<std::future<int>> fut;
+    ThreadPool pool(8);
     for(int i = 0; i < 8; ++i) {
-        results.emplace_back(
-            pool.enqueue([i] {
+        fut.emplace_back(
+            pool.enqueue([i]{
                 std::cout << "hello " + std::to_string(i) << std::endl;
                 std::this_thread::sleep_for(std::chrono::seconds(1));
                 std::cout << "world " + std::to_string(i) << std::endl;
                 return i*i;
-            })
-        );
+        }));
     }
-    std::cout << "result" << std::endl;
-    for(auto && result: results)
-        std::cout << result.get() << " ";
-    
+    // std::for_each(fut.begin(), fut.end(), [](auto & el){ std::cout << el.get() << std::endl;});
+    for (auto && el : fut) 
+    {
+        std::cout << el.get() << std::endl;
+    }
     return 0;
 }
