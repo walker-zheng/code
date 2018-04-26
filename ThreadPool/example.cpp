7be47bae07fbdbfbaf2e990bfe31cb8f5c92fe74
walker-zheng <mykulou@gmail.com>
2018-04-26 15:19:05 +0800

    update astyle

--- ThreadPool/example.cpp
+++ ThreadPool/example.cpp
@@ -9,10 +9,12 @@
 
 #include "ThreadPool.h"
 
-int sum_all(int n){
+int sum_all(int n)
+{
     int sum{0};
-    for(auto i = 0; i != n; i++)
-        sum +=i;
+    for(auto i = 0; i != n; i++) {
+        sum += i;
+    }
     return sum;
 }
 int main()
@@ -20,17 +22,15 @@ int main()
     std::vector<std::future<int>> fut;
     ThreadPool pool(8);
     for(int i = 0; i < 8; ++i) {
-        fut.emplace_back(
-            pool.enqueue([i]{
-                std::cout << "hello " + std::to_string(i) << std::endl;
-                std::this_thread::sleep_for(std::chrono::seconds(1));
-                std::cout << "world " + std::to_string(i) << std::endl;
-                return i*i;
+        fut.emplace_back(pool.enqueue([i]() {
+            std::cout << "hello " + std::to_string(i) << std::endl;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            std::cout << "world " + std::to_string(i) << std::endl;
+            return i * i;
         }));
     }
     // std::for_each(fut.begin(), fut.end(), [](auto & el){ std::cout << el.get() << std::endl;});
-    for (auto && el : fut) 
-    {
+    for(auto && el : fut) {
         std::cout << el.get() << std::endl;
     }
     return 0;
--- test/qsort.cpp
+++ test/qsort.cpp
@@ -4,39 +4,33 @@
 #include <vector>
 #include <forward_list>
 
-    template <class ForwardIt>
+template <class ForwardIt>
 void quicksort(ForwardIt first, ForwardIt last)
 {
-    if(first == last) return;
-    auto pivot = *std::next(first, std::distance(first,last)/2);
-    ForwardIt middle1 = std::partition(first, last, 
-            [pivot](const auto& em){ return em < pivot; });
-    ForwardIt middle2 = std::partition(middle1, last, 
-            [pivot](const auto& em){ return !(pivot < em); });
+    if(first == last) {return;}
+    auto pivot = *std::next(first, std::distance(first, last) / 2);
+    ForwardIt middle1 = std::partition(first, last, [pivot](const auto & em) {return em < pivot;});
+    ForwardIt middle2 = std::partition(middle1, last, [pivot](const auto & em) {return !(pivot < em);});
     quicksort(first, middle1);
     quicksort(middle2, last);
 }
 
 int main()
 {
-    std::vector<int> v = {0,1,2,3,4,5,6,7,8,9};
+    std::vector<int> v = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     std::cout << "Original vector:\n    ";
-    for (int elem : v) std::cout << elem << ' ';
-
-    auto it = std::partition(v.begin(), v.end(), [](int i){return i % 2 == 0;});
-
+    for(int elem : v) {std::cout << elem << ' ';}
+    auto it = std::partition(v.begin(), v.end(), [](int i) {return i % 2 == 0;});
     std::cout << "\nPartitioned vector:\n    ";
-    std::copy(std::begin(v), it, std::ostream_iterator<int>(std::cout, " "));
+    std::copy(std::begin(v), it, std::ostream_iterator<int> (std::cout, " "));
     std::cout << " * ";
-    std::copy(it, std::end(v), std::ostream_iterator<int>(std::cout, " "));
-
+    std::copy(it, std::end(v), std::ostream_iterator<int> (std::cout, " "));
     std::forward_list<int> fl = {1, 30, -4, 3, 5, -4, 1, 6, -8, 2, -5, 64, 1, 92};
     std::cout << "\nUnsorted list:\n    ";
-    for(int n : fl) std::cout << n << ' ';
-    std::cout << '\n';  
-
+    for(int n : fl) {std::cout << n << ' ';}
+    std::cout << "\nSorted list:\n    ";
     quicksort(std::begin(fl), std::end(fl));
-    std::cout << "Sorted using quicksort:\n    ";
-    for(int fi : fl) std::cout << fi << ' ';
+    // std::cout << "Sorted using quicksort:\n    ";
+    for(int fi : fl) {std::cout << fi << ' ';}
     std::cout << '\n';
 }
